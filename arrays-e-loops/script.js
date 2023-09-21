30bd47e68a35234415c3e266c54e65743267fa23
pragma <diogosouza03@gmail.com>
2023-09-21 13:19:46 -0400

    arrays-e-loops

---
+++ arrays-e-loops/script.js
@@ -0,0 +1,10 @@
+var videoGames = ['Switch', 'PS4', 'XBOX'];
+
+// var ultimoItem = videoGames.pop();
+
+videoGames.push('3DS');
+
+
+for (var numero = 0; numero <= 100; numero+2){
+  console.log(numero);
+}
